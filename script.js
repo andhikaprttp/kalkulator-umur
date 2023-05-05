28c6959c6e85f19d6cef954d1202cdc5b216f626
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-05 12:14:59 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 function hitungUmur() {
-			var tanggalLahir = new Date(document.getElementById("tanggal-lahir").value);
-			var hariIni = new Date();
+  var tanggalLahir = new Date(document.getElementById("tanggal-lahir").value);
+  var hariIni = new Date();
 
-			var tahun = hariIni.getFullYear() - tanggalLahir.getFullYear();
-			var bulan = hariIni.getMonth() - tanggalLahir.getMonth();
-			var hari = hariIni.getDate() - tanggalLahir.getDate();
+  var tahun = hariIni.getFullYear() - tanggalLahir.getFullYear();
+  var bulan = hariIni.getMonth() - tanggalLahir.getMonth();
+  var hari = hariIni.getDate() - tanggalLahir.getDate();
 
-			if (bulan < 0 || (bulan === 0 && hari < 0)) {
-				tahun--;
-				bulan += 12;
-			}
+  if (bulan < 0 || (bulan === 0 && hari < 0)) {
+    tahun--;
+    bulan += 12;
+  }
 
-			document.getElementById("hasil-umur").innerHTML = "Umur Anda adalah " + tahun + " tahun, " + bulan + " bulan, dan " + hari + " hari.";
-		}
+  document.getElementById("hasil-umur").innerHTML =
+    "Umur Anda adalah " +
+    tahun +
+    " tahun, " +
+    bulan +
+    " bulan, dan " +
+    hari +
+    " hari.";
+}
